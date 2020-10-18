d407c8b4d3042c716b4b2c9fc66f464905c40207
Nicholas Kolatsis <nkolatsis@gmail.com>
2020-10-18 15:07:28 +0200

    Initial commit

---
+++ src/test/java/com/nk/hbs/NativeTimestampResourceIT.java
@@ -0,0 +1,9 @@
+package com.nk.hbs;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeTimestampResourceIT extends TimestampResourceTest {
+
+    // Execute the same tests but in native mode.
+}
